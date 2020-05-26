dac6f7ee0a4257b61f7a5ea91996c60258c04d99
h1dd3n01 <h1dd3n1@protonmail.com>
2020-05-27 02:02:55 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment }  from 'react';
+import Exercises from './Components/exercises/'
 
+// import {Header, Footer} from './Components/layouts/'
+import Header from "./Components/layouts/Header";
+import Footer from "./Components/layouts/Footer";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header/>
+      <Exercises/>
+      <Footer/>
+
+    </Fragment>
+
+
   );
 }
 
---
+++ src/Components/layouts/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+export default () => {
+    return(
+        <footer>Footer</footer>
+    )
+}
